bc623bd914f1fb49ebf457a13b950070148b4d91
0Batty <56723850+0Batty@users.noreply.github.com>
2021-05-12 00:47:17 +0200

    pridej sql update kryple

--- index.js
+++ index.js
@@ -1,46 +1,56 @@
 const http = require('express');
 var path = require('path');
-var sqlite3 = require('sqlite3').verbose();
+const fs = require('fs');
+var sqlite3 = require('sqlite3').verbose(); //https://www.sqlitetutorial.net/sqlite-update/
 var db = new sqlite3.Database('data.db');
 
 const app = http();
 
-app.use(http.urlencoded({extended: true}));
+app.use(http.urlencoded({ extended: true }));
 app.use(http.json());
 
-app.post('/win',(req,res)=>{
+app.post('/win', (req, res) => {
     console.log("vyhra");
+    db
     res.redirect('./');
 });
 
-app.post('/draw',(req,res)=>{
+app.post('/draw', (req, res) => {
     console.log("remiza");
     res.redirect('./');
 });
 
-app.post('/loss',(req,res)=>{
+app.post('/loss', (req, res) => {
     console.log("prohra");
     res.redirect('./');
 });
 
-app.post('/test',(req,res)=>{
-    console.log("test");
-    res.redirect('./');
+app.post('/init', (req, res) => {
+    if (fs.existsSync("data.db") && fs.existsSync("db-created.nothing")) {
+        console.log("init_fileexists");
+        res.redirect('./');
+    } else {
+        console.log("init");
+        db.serialize(() => {
+            db.run('CREATE TABLE wins (win_count INT)');
+            db.run('CREATE TABLE draw (draw_count INT)');
+            db.run('CREATE TABLE loss (loss_count INT)');
+        });
+        try {
+            fs.writeFileSync("db-created.nothing", "");
+        } catch (error) {
+            console.log(error);
+        }
+
+        res.redirect('./');
+    }
 });
 
-app.post('/init',(req,res)=>{
-    console.log("init");
-    db.serialize(()=>{
-        db.run('CREATE TABLE data (win_count INT, draw_count INT, loss_count INT)');
-    });
-    res.redirect('./');
-});
-
-app.get('/',(req, res)=>{
+app.get('/', (req, res) => {
     res.sendFile('./index.html', { root: path.join(__dirname, './') });
 })
 
 
-app.listen('80', ()=>{
+app.listen('80', () => {
     console.log("server bezi");
 })

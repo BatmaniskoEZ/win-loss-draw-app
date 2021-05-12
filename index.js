d714d07479a7bfcd1222f13af4db3011b37ff470
0Batty <56723850+0Batty@users.noreply.github.com>
2021-05-12 13:38:03 +0200

    ez funguje

--- index.js
+++ index.js
@@ -1,40 +1,60 @@
 const http = require('express');
+const { app, BrowserWindow } = require('electron')
 var path = require('path');
 const fs = require('fs');
-var sqlite3 = require('sqlite3').verbose(); //https://www.sqlitetutorial.net/sqlite-update/
-var db = new sqlite3.Database('data.db');
+const sqlite3 = require('sqlite3').verbose(); //https://www.sqlitetutorial.net/sqlite-update/
+let db = new sqlite3.Database('data.db');
 
-const app = http();
+//kod pro express post/get
+const expressApp = http();
 
-app.use(http.urlencoded({ extended: true }));
-app.use(http.json());
+expressApp.use(http.urlencoded({ extended: true }));
+expressApp.use(http.json());
+expressApp.use(http.static(__dirname));
 
-app.post('/win', (req, res) => {
+expressApp.post('/reset', (req, res) => {
+    console.log("reset");
+    db.serialize(()=>{
+        db.run('UPDATE stats SET win_count = 0 WHERE id = 1');
+        db.run('UPDATE stats SET draw_count = 0 WHERE id = 1');
+        db.run('UPDATE stats SET loss_count = 0 WHERE id = 1');
+    });
+    res.redirect('./');
+});
+
+expressApp.post('/win', (req, res) => {
     console.log("vyhra");
-    db
+    db.serialize(()=>{
+        db.run('UPDATE stats SET win_count = win_count+1 WHERE id = 1');
+    });
     res.redirect('./');
 });
 
-app.post('/draw', (req, res) => {
+expressApp.post('/draw', (req, res) => {
     console.log("remiza");
+    db.serialize(()=>{
+        db.run('UPDATE stats SET draw_count = draw_count+1 WHERE id = 1');
+    });
     res.redirect('./');
 });
 
-app.post('/loss', (req, res) => {
+expressApp.post('/loss', (req, res) => {
     console.log("prohra");
+    db.serialize(()=>{
+        db.run('UPDATE stats SET loss_count = loss_count+1 WHERE id = 1');
+    });
     res.redirect('./');
 });
 
-app.post('/init', (req, res) => {
+expressApp.post('/init', (req, res) => {
     if (fs.existsSync("data.db") && fs.existsSync("db-created.nothing")) {
-        console.log("init_fileexists");
+        console.log("init_prepared_already");
         res.redirect('./');
     } else {
         console.log("init");
         db.serialize(() => {
-            db.run('CREATE TABLE wins (win_count INT)');
-            db.run('CREATE TABLE draw (draw_count INT)');
-            db.run('CREATE TABLE loss (loss_count INT)');
+            db.run('CREATE TABLE stats (id INTEGER NOT NULL DEFAULT 1, win_count INTEGER NOT NULL DEFAULT 0, draw_count INTEGER NOT NULL DEFAULT 0, loss_count INTEGER NOT NULL DEFAULT 0, PRIMARY KEY(id));');
+            db.run('INSERT INTO stats VALUES(1,0,0,0)');
         });
         try {
             fs.writeFileSync("db-created.nothing", "");
@@ -46,11 +66,11 @@ app.post('/init', (req, res) => {
     }
 });
 
-app.get('/', (req, res) => {
+expressApp.get('/', (req, res) => {
     res.sendFile('./index.html', { root: path.join(__dirname, './') });
 })
 
 
-app.listen('80', () => {
+expressApp.listen('80', () => {
     console.log("server bezi");
 })

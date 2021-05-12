3ffe581a0ce95778506195e1d9b657973c10a0a5
0Batty <56723850+0Batty@users.noreply.github.com>
2021-05-12 17:44:09 +0200

    funkcni elektron appka v0.1.1

--- index.js
+++ index.js
@@ -2,8 +2,44 @@ const http = require('express');
 const { app, BrowserWindow } = require('electron')
 var path = require('path');
 const fs = require('fs');
-const sqlite3 = require('sqlite3').verbose(); //https://www.sqlitetutorial.net/sqlite-update/
+const sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database('data.db');
+
+//elektron kod
+
+function createWindow () {
+    const win = new BrowserWindow({
+      width: 465,
+      height: 135
+    })
+    win.removeMenu()
+    win.loadURL('http://localhost:90/index.html')
+    /*win.on('resized', function () {
+        var size   = win.getSize();
+        var width  = size[0];
+        var height = size[1];
+        console.log("width: " + width);
+        console.log("height: " + height);
+    });*/
+    win.setResizable(false);
+    //console.log(__dirname);
+  }
+  
+  app.whenReady().then(() => {
+    createWindow()
+  
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+      }
+    })
+  })
+  
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
 
 //kod pro express post/get
 const expressApp = http();
@@ -14,7 +50,7 @@ expressApp.use(http.static(__dirname));
 
 expressApp.post('/reset', (req, res) => {
     console.log("reset");
-    db.serialize(()=>{
+    db.serialize(() => {
         db.run('UPDATE stats SET win_count = 0 WHERE id = 1');
         db.run('UPDATE stats SET draw_count = 0 WHERE id = 1');
         db.run('UPDATE stats SET loss_count = 0 WHERE id = 1');
@@ -24,7 +60,7 @@ expressApp.post('/reset', (req, res) => {
 
 expressApp.post('/win', (req, res) => {
     console.log("vyhra");
-    db.serialize(()=>{
+    db.serialize(() => {
         db.run('UPDATE stats SET win_count = win_count+1 WHERE id = 1');
     });
     res.redirect('./');
@@ -32,7 +68,7 @@ expressApp.post('/win', (req, res) => {
 
 expressApp.post('/draw', (req, res) => {
     console.log("remiza");
-    db.serialize(()=>{
+    db.serialize(() => {
         db.run('UPDATE stats SET draw_count = draw_count+1 WHERE id = 1');
     });
     res.redirect('./');
@@ -40,7 +76,7 @@ expressApp.post('/draw', (req, res) => {
 
 expressApp.post('/loss', (req, res) => {
     console.log("prohra");
-    db.serialize(()=>{
+    db.serialize(() => {
         db.run('UPDATE stats SET loss_count = loss_count+1 WHERE id = 1');
     });
     res.redirect('./');
@@ -70,7 +106,17 @@ expressApp.get('/', (req, res) => {
     res.sendFile('./index.html', { root: path.join(__dirname, './') });
 })
 
+expressApp.get('/getdata', (req, res) => {
+    db.serialize(() => {
+        db.get('SELECT * FROM stats WHERE id = 1', (err, row) => {
+            //console.log(row);
+            res.send({win: row.win_count, draw: row.draw_count, loss: row.loss_count });
+        });
+    });
+    //res.send({win: 0, draw: 0, loss: 0});
+})
 
-expressApp.listen('80', () => {
-    console.log("server bezi");
+
+expressApp.listen('90', () => {
+    //console.log("server bezi");
 })
--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 console.log("app.js loaded");
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('/getdata').then(res => res.json()).then(data => {
+        console.log(data);
+        document.getElementById('Wins').innerHTML=data.win;
+        document.getElementById('Draws').innerHTML=data.draw;
+        document.getElementById('Losses').innerHTML=data.loss;
+    });
+}, false);
